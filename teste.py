1dee27127476b487ad973f9eeb2826d807598fa6
Ricardo Rodrigues Rapozo <92274024+Ricardo-Rodrigues-Rapozo@users.noreply.github.com>
2024-10-27 07:42:11 -0300

    Update teste.py
    codigo que envia um dado via UART e plota um grafico a partir de um vetor recebido via UART

--- teste.py
+++ teste.py
@@ -1,46 +1,32 @@
 import serial
-import struct
 import time
-import senoide
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Parâmetros da senoide
-A = 1        # Amplitude
-f = 1        # Frequência (Hz)
-Fs = 100     # Frequência de amostragem (Hz)
-phi = 0      # Fase (radianos)
+#Função para enviar sinal contínuo para o microcontrolador
+def send_sync_signal():
+    sync_byte = bytes([1])  # Envia o byte 0xAA
+    ser.write(sync_byte)
+    print("Sinal de sincronização enviado")
+    time.sleep(1)  # Espera 1 segundo antes de enviar o próximo sinal
 
-# Gera a senoide
-y = senoide.senoide(A, f, Fs, phi)
+# Configura a porta serial
+ser = serial.Serial('COM3', 115200, timeout=None)
+ser.flush() ## limpa dados pendentes na porta serial 
+time.sleep(1)  # Espera a conexão ser estabelecida
 
-# Configurar a porta serial (ajuste o nome da porta conforme necessário)
-ser = serial.Serial('COM3', 115200, timeout=1)  # Use 'COM3' no Windows, '/dev/ttyUSB0' ou '/dev/ttyACM0' no Linux
+# Cria um vetor de zeros para armazenar 1000 valores uint16
+vect = np.zeros(1000, dtype=np.uint16)
+valor_inteiro = 1
+data = []
+BUFFER_SIZE = 1000
 
-def send_float(value):
-    # Envia um número de ponto flutuante (float) de 32 bits pela UART
-    data = struct.pack('<f', value)  # '<f' significa float de 4 bytes (little-endian)
-    ser.write(data)
-
-def receive_int32():
-    # Recebe um número inteiro de 32 bits pela UART
-    data = ser.read(4)  # Lê 4 bytes
-    if len(data) == 4:
-        return struct.unpack('<I', data)[0]  # Converte de volta para um inteiro
-    else:
-        return None
-
-try:
-    for value in y:
-        # Envia cada valor da senoide como um float de 32 bits
-        send_float(value)
-        time.sleep(0.01)  # Ajuste o tempo conforme necessário para sincronizar a comunicação
-
-    # Após enviar todos os valores, você pode ler um número do microcontrolador
-    received = receive_int32()
-    if received is not None:
-        print(f"Received: {received}")
-    else:
-        print("No data received")
-
-except KeyboardInterrupt:
-    ser.close()
-    print("Serial connection closed.")
+while True:    
+    send_sync_signal()
+    if ser.in_waiting > (4*BUFFER_SIZE-1):
+        data = np.frombuffer(ser.read(BUFFER_SIZE*4),dtype=np.int32)
+        plt.plot(data)
+        plt.title("Dados Serial")
+        plt.xlabel("Índice")
+        plt.ylabel("Valor")
+        plt.show()
